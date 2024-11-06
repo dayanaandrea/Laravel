3c0aed330567ee925d3d9411729300d414d36435
dayanaandrea <dayanaandrea.fernandezla@elorrieta-errekamari.com>
2024-11-06 14:54:12 +0000

    Primer commit

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::controller(PostController::class)->group(function () {
+    Route::get('/posts', 'index')->name('posts.index');
+    Route::get('/posts/{post}', 'show')->name('posts.show');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
