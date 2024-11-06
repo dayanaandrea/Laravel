c4fe8f41755f1f43ac9114f3ca57289c66abe488
dayanaandrea <dayanaandrea.fernandezla@elorrieta-errekamari.com>
2024-11-06 15:58:59 +0000

    Se actualiza crea pantalla create

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@ Route::get('/', function () {
 Route::controller(PostController::class)->group(function () {
     Route::get('/posts', 'index')->name('posts.index');
     Route::get('/posts/{post}', 'show')->name('posts.show');
+    Route::get('/posts/create', 'create')->name('posts.create');
+
+    Route::post('/posts/store', 'store')->name('posts.store');
 });
 
 Auth::routes();
