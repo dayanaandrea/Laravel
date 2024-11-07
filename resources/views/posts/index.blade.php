f29ef6cb48943d42ad25220633805bb5738e6120
dayanaandrea <dayanaandrea.fernandezla@elorrieta-errekamari.com>
2024-11-07 17:48:21 +0000

    Se annade doc.edit y se modifican datos

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,11 +1,24 @@
-
-<ul>
-  {{--esto es un comentario: recorremos el listado de posts--}}
-  @foreach ($posts as $post)
-    {{-- visualizamos los atributos del objeto --}}
-    <li>
-      <a href="{{route('posts.show',$post)}}"> {{$post->titulo}}</a>
+@extends('layouts.app')
+@section('content')
+<div class="container">
+@foreach ($posts as $post)
+  {{-- visualizamos los atributos del objeto --}}
+  <li class="pt-1">
+    <div class="d-flex flex-row">
+      <a href="{{route('posts.show',$post)}}"> {{$post->titulo}}</a>.
       Escrito el {{$post->created_at}}
-    </li>
-  @endforeach
-</ul>
+      <a class="btn btn-warning btn-sm" href="{{route('posts.edit',$post)}}"
+        role="button">Editar</a>
+
+      <form action="{{route('posts.destroy',$post)}}" method="POST">
+        @csrf
+        @method('DELETE')
+        <button class="btn btn-sm btn-danger" type="submit"
+          onclick="return confirm('Are you sure?')">Delete
+        </button>
+      </form>
+    </div>
+  </li>
+@endforeach
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,10 @@ Route::controller(PostController::class)->group(function () {
     Route::get('/posts', 'index')->name('posts.index');
     Route::get('/posts/{post}', 'show')->name('posts.show');
     Route::get('/posts/create', 'create')->name('posts.create');
-
+    Route::post('/posts/edit', 'edit')->name('posts.edit');
     Route::post('/posts/store', 'store')->name('posts.store');
+
+    Route::delete('/posts/destroy', 'destroy')->name('posts.destroy');
 });
 
 Auth::routes();
