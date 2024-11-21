4cc7cd3063dfb1d3783ee7e6ab76a6d799f55784
Dayana <elorrieta@104-04.elorrieta.int>
2024-11-21 17:49:18 +0100

    Crear controlador de autenticación

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\PostController;
+use App\Http\Controllers\API\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,3 +12,7 @@ Route::get('/user', function (Request $request) {
 Route::apiresources([
     'posts' => PostController::class,
 ]);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->middleware('auth:sanctum');
