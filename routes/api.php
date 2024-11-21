dc5170ef1831c199feac7b59a4dd74015b5c2f04
Dayana <elorrieta@104-04.elorrieta.int>
2024-11-21 17:31:59 +0100

    Crear controladores específicos

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
+use App\Http\Controllers\API\PostController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
