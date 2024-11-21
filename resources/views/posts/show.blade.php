6349da7d4c601289a56a863848d4a02703c627bc
Dayana <elorrieta@104-04.elorrieta.int>
2024-11-21 18:22:27 +0100

    Se añade un foreach para poder ver los comentarios y la fecha de creación

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,30 +1,39 @@
 @extends('layouts.app')
+
 @section('content')
 <div class="container">
   <h1>{{$post->titulo}}</h1>
   <p>Creado el {{$post->created_at}}</p>
   <p>{{$post->texto}}</p>
-  @auth
   <div>
-    <h3>Añadir comentario</h3>
-    <form class="mt-2" name="create_platform"
-      action="{{route('comentarios.store')}}" method="POST" 
-      enctype="multipart/form-data">
-      
-      @csrf
-      <input type="text" class="form-control" id="post" name="post" 
-        required hidden value="{{$post->id}}"/>
+    <h4>Comentarios</h4>
+      <ul>
+        @foreach ($post->comentarios as $comentario)
+          {{-- visualizamos los atributos del objeto --}}
+          <li class="pt-1">
+              {{$comentario->texto}}.
 
-      <div class="form-group mb-3">
-        <textarea type="textarea" rows="5" class="form-control" 
-            id="texto" name="texto"></textarea>
-      </div>
-      <button type="submit" class="btn btn-primary" name="">
-        Comenta!
-      </button>
-    </form>
+              Escrito el {{$post->created_at}}
+          </li>
+        @endforeach
+      </ul>
+
+      <br/><br/>
+      @auth
+        <div>
+            <h3>Añadir comentario</h3>
+            <form class="mt-2" name="create_platform"
+              action="{{route('comentarios.store')}}" method="POST" enctype="multipart/form-data">
+              @csrf
+              <input type="text" class="form-control" id="post" name="post" required hidden value="{{$post->id}}"/>
+              <div class="form-group mb-3">
+                <textarea type="textarea" rows="5" class="form-control" id="texto" name="texto"></textarea>
+              </div>
+              <button type="submit" class="btn btn-primary" name="">Comenta!</button>
+            </form>
+        </div>
+      @endauth
   </div>
-@endauth
-
 </div>
+
 @endsection
